94f35b19b300548ff33a35972edcca969b880198
Matthias Osswald <m@osswald.li>
2019-08-28 13:14:28 +0200

    Add initial bobtemplate generated package.

---
+++ ftw/whocando/__init__.py
@@ -0,0 +1,8 @@
+from zope.i18nmessageid import MessageFactory
+
+
+_ = MessageFactory('ftw.whocando')
+
+
+def initialize(context):
+    pass
---
+++ ftw/whocando/interfaces.py
@@ -0,0 +1,5 @@
+from zope.interface import Interface
+
+
+class IFtwWhocandoLayer(Interface):
+    """Request layer for ftw.whocando"""
